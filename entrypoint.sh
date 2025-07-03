472dbc4d76547dcf6e4c2d0d8947eff85f443db4
Matías Cariboni <mcariboni@icloud.com>
2025-07-03 15:40:19 +0200

    next solution...

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,8 +67,8 @@ CLOUDFRONT_DIST_ID=$(echo "$REPO_VARS" | jq -r --arg key "$full_var_name" '.[$ke
 # If the variable is still missing or null
 if [ -z "$CLOUDFRONT_DIST_ID" ] || [[ "$CLOUDFRONT_DIST_ID" == "null" ]]; then
   echo "CLOUDFRONT_DIST_ID not found in REPO_VARS or REPO_SECRETS."
-  echo 'NOCDNFOUND'
+  CLOUDFRONT_DIST_ID="NOCDNFOUND"
 fi
 
 # Exporting CloudFront Distribution
-echo "$CLOUDFRONT_DIST_ID"
+echo "CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID" >> "$GITHUB_OUTPUT"
